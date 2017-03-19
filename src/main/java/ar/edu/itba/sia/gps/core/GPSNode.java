3564feb736349b62b554631dda6d96849e59bc2a
Matias Comercio <mcomerciovazquez@gmail.com>
2017-03-19 14:53:04 -0300

    Fix bugs, style and other related code issues
    - Fix package structure and indentation issues
    - Fix GPSEngine
      - Improve GPSEngine bestCostsStates
        - It was implemented as a Set but never really compare the costs of
          reaching those states. It was simply ignoring already reached states
          withour that comparison
        - Add some TODOs that will be resolved after asking Cristina & Alan
          if those changes are possible
    - Remove unnecesary `equal` method on GPSSate interface
    - Update .gitignore

---
+++ src/main/java/ar/edu/itba/sia/gps/core/GPSNode.java
@@ -0,0 +1,65 @@
+package ar.edu.itba.sia.gps.core;
+
+import ar.edu.itba.sia.gps.api.GPSState;
+
+import java.util.Objects;
+
+public class GPSNode {
+  private final GPSState state;
+  private final int cost;
+  private GPSNode parent;
+
+  /**
+   * Create a new GPS node
+   * @param state The non-null state associated with the node being created
+   * @param cost The cost to reach the current node
+   * @exception NullPointerException if {@code state} is null
+   */
+  public GPSNode(final GPSState state, final Integer cost) {
+    this.state = Objects.requireNonNull(state);
+    this.cost = cost;
+  }
+
+  public GPSNode getParent() {
+    return parent;
+  }
+
+  public void setParent(final GPSNode parent) {
+    this.parent = parent;
+  }
+
+  public GPSState getState() {
+    return state;
+  }
+
+  public int getCost() {
+    return cost;
+  }
+
+  @Override
+  public String toString() {
+    return state.toString();
+  }
+
+  public String getSolution() {
+    if (this.parent == null) {
+      return this.state.toString();
+    }
+    return this.parent.getSolution() + this.state.toString();
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (!(o instanceof GPSNode)) return false;
+
+    final GPSNode gpsNode = (GPSNode) o;
+
+    return state.equals(gpsNode.state);
+  }
+
+  @Override
+  public int hashCode() {
+    return state.hashCode();
+  }
+}
---
+++ src/main/java/ar/edu/itba/sia/gps/exception/NotApplicableException.java
@@ -0,0 +1,5 @@
+package ar.edu.itba.sia.gps.exception;
+
+public class NotApplicableException extends Exception {
+  private static final long serialVersionUID = -4084061417773665019L;
+}
