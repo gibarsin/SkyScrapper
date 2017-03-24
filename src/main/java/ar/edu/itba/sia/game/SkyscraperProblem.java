53b91fed523d304081143211f71f95143e955f61
Juan <jpmrno@gmail.com>
2017-03-24 18:12:09 -0300

    add SkyScraperProblem

---
+++ src/main/java/ar/edu/itba/sia/game/SkyscraperProblem.java
@@ -0,0 +1,193 @@
+package ar.edu.itba.sia.game;
+
+import ar.edu.itba.sia.gps.api.GPSProblem;
+import ar.edu.itba.sia.gps.api.GPSRule;
+import ar.edu.itba.sia.gps.api.GPSState;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class SkyscraperProblem implements GPSProblem {
+
+  private final SkyscraperState initialState;
+
+  public SkyscraperProblem(final SkyscraperBoard initialBoard) {
+    this.initialState = new SkyscraperState(Objects.requireNonNull(initialBoard));
+  }
+
+  @Override
+  public GPSState getInitState() {
+    return initialState;
+  }
+
+  @Override
+  public boolean isGoal(final GPSState state) {
+    final SkyscraperState ssState = (SkyscraperState) state;
+    final SkyscraperBoard board = ssState.getBoard();
+
+    return isFull(board) && !areCrossConflicts(board) && !areVisibilityConflicts(board);
+  }
+
+  private boolean areVisibilityConflicts(final SkyscraperBoard board) {
+    return IntStream.range(0, board.getSize() - 1).parallel() // TODO Test
+        .anyMatch(v -> !hasCorrectRowVisibility(board, v) || !hasCorrectColumnVisibility(board, v));
+  }
+
+  private boolean hasCorrectRowVisibility(final SkyscraperBoard board, final int row) {
+    int maxHeight = 0;
+    int seen = 0;
+
+    if (board.hasVisibility(Border.LEFT, row)) {
+      final int visibility = board.getVisibility(Border.LEFT, row);
+      for (int j = 0; j < board.getSize(); j++) {
+        final int currValue = board.getValue(row, j);
+
+        if (currValue > maxHeight) {
+          seen++;
+          maxHeight = currValue;
+
+          if(seen > visibility) {
+            return false;
+          }
+        }
+      }
+    }
+
+    if (board.hasVisibility(Border.RIGHT, row)) {
+      final int visibility = board.getVisibility(Border.RIGHT, row);
+      for (int j = board.getSize() - 1; j >= 0; j--) {
+        final int currValue = board.getValue(row, j);
+
+        if (currValue > maxHeight) {
+          seen++;
+          maxHeight = currValue;
+
+          if(seen > visibility) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private boolean hasCorrectColumnVisibility(final SkyscraperBoard board, final int column) {
+    int maxHeight = 0;
+    int seen = 0;
+
+    if (board.hasVisibility(Border.TOP, column)) {
+      final int visibility = board.getVisibility(Border.TOP, column);
+      for (int i = 0; i < board.getSize(); i++) {
+        final int currValue = board.getValue(i, column);
+
+        if (currValue > maxHeight) {
+          seen++;
+          maxHeight = currValue;
+
+          if(seen > visibility) {
+            return false;
+          }
+        }
+      }
+    }
+
+    if (board.hasVisibility(Border.BOTTOM, column)) {
+      final int visibility = board.getVisibility(Border.BOTTOM, column);
+      for (int i = board.getSize() - 1; i >= 0; i--) {
+        final int currValue = board.getValue(i, column);
+
+        if (currValue > maxHeight) {
+          seen++;
+          maxHeight = currValue;
+
+          if(seen > visibility) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private boolean isFull(final SkyscraperBoard board) {
+    for (int i = 0; i < board.getSize(); i++) {
+      for (int j = 0; j < board.getSize(); j++) {
+        if (board.isEmpty(i, j)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private boolean areCrossConflicts(final SkyscraperBoard board) {
+    for (int i = 0; i < board.getSize(); i++) {
+      for (int j = 0; j < board.getSize(); j++) {
+        if (!isUniqueInRow(board, i, j) || !isUniqueInColumn(board, i, j)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public List<GPSRule> getRules() {
+    return null;
+  }
+
+  @Override
+  public Integer getHValue(final GPSState state) {
+    return null;
+  }
+
+  /**
+   * This method assumes that none of the values in the board are empty
+   *
+   * @return true if the value is unique in its column; false else
+   */
+  private boolean isUniqueInColumn(final SkyscraperBoard board, final int row, final int col) {
+    final int value = board.getValue(row, col);
+
+    for (int i = 0; i < row; i++) {
+      if (value == board.getValue(i, col)) {
+        return false;
+      }
+    }
+
+    for (int i = row + 1; i < board.getSize(); i++) {
+      if (value == board.getValue(i, col)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * This method assumes that none of the values in the board are empty
+   *
+   * @return true if the value is unique in its row; else false
+   */
+  private boolean isUniqueInRow(final SkyscraperBoard board, final int row, final int col) {
+    final int value = board.getValue(row, col);
+
+    for (int j = 0; j < col; j++) {
+      if (!board.isEmpty(row, j) && value == board.getValue(row, j)) {
+        return false;
+      }
+    }
+
+    for (int j = col + 1; j < board.getSize(); j++) {
+      if (!board.isEmpty(row, j) && value == board.getValue(row, j)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
