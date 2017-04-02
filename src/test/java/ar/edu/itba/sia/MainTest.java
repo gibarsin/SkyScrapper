e438433052cb74c8e4d0d1d1d23ee09b9346fe0c
MatiasComercio <MatiasComercio@users.noreply.github.com>
2017-04-02 16:28:08 -0300

    Add SkyscraperJavaxUI & improve some tests (#9)
    - Change 6x6 test matrix
    - Add null assert for solution in tests
    - Add Skyscraper javafx ui display board
    - Fix close scanners issue on InputSerializerHelper
    - Add effects & buttons on ui to be easy to used
    - Add skyscraper gui when solution is not found

--- src/test/java/ar/edu/itba/sia/MainTest.java
+++ src/test/java/ar/edu/itba/sia/MainTest.java
@@ -6,6 +6,10 @@ import ar.edu.itba.sia.gps.core.GPSEngine;
 import ar.edu.itba.sia.gps.strategy.SearchStrategy;
 import ar.edu.itba.sia.matrix.Matrix4HardDataPut;
 import ar.edu.itba.sia.matrix.Matrix4HardDataSwap;
+import ar.edu.itba.sia.matrix.Matrix5HardDataPut;
+import ar.edu.itba.sia.matrix.Matrix6HardDataPut;
+import ar.edu.itba.sia.matrix.Matrix9HardDataPut;
+import ar.edu.itba.sia.matrix.Matrix9HardDataSwap;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Assert;
@@ -16,6 +20,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class MainTest {
+
   private final String name;
   private final GPSProblem problem;
   private final GPSState solvedState;
@@ -34,52 +39,63 @@ public class MainTest {
   public static Collection<Object[]> data() {
     return Arrays.asList(
         new Matrix4HardDataPut().getData(),
-        new Matrix4HardDataSwap().getData()
-//        new Matrix9HardDataSwap().getData()
-//        Matrix5HardData.getData()
-//        Matrix6HardData.getData(),
-//        Matrix9HardData.getData()
+        new Matrix4HardDataSwap().getData(),
+        new Matrix5HardDataPut().getData(),
+        new Matrix6HardDataPut().getData(),
+        new Matrix9HardDataPut().getData(),
+        new Matrix9HardDataSwap().getData()
     );
   }
 
-//  @Test
-//  public void testBFS() throws Exception {
-//    if (method.equals("PUT")) {
-//      final GPSEngine engine = new GPSEngine(problem, SearchStrategy.BFS);
-//      engine.findSolution();
-//      Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
-//    }
-//  }
-//
-//  @Test
-//  public void testDFS() throws Exception {
-//    if (method.equals("PUT")) {
-//      final GPSEngine engine = new GPSEngine(problem, SearchStrategy.DFS);
-//      engine.findSolution();
-//      Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
-//    }
-//  }
+  @Test
+  public void testBFS() throws Exception {
+    if (!method.equals("PUT")) {
+      return;
+    }
+
+    final GPSEngine engine = new GPSEngine(problem, SearchStrategy.BFS);
+    engine.findSolution();
+    Assert.assertNotNull(engine.getSolutionNode());
+    Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
+  }
+
+  @Test
+  public void testDFS() throws Exception {
+    if (!method.equals("PUT")) {
+      return;
+    }
+
+    final GPSEngine engine = new GPSEngine(problem, SearchStrategy.DFS);
+    engine.findSolution();
+    Assert.assertNotNull(engine.getSolutionNode());
+    Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
+  }
 
   @Test
   public void testIDDFS() throws Exception {
-    if (method.equals("PUT")) {
-      final GPSEngine engine = new GPSEngine(problem, SearchStrategy.IDDFS);
-      engine.findSolution();
-      Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
+    if (!method.equals("PUT")) {
+      return;
     }
+
+    final GPSEngine engine = new GPSEngine(problem, SearchStrategy.IDDFS);
+    engine.findSolution();
+    Assert.assertNotNull(engine.getSolutionNode());
+    Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
   }
 
   @Test
   public void testGREEDY() throws Exception {
     final GPSEngine engine = new GPSEngine(problem, SearchStrategy.GREEDY);
     engine.findSolution();
+    Assert.assertNotNull(engine.getSolutionNode());
     Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
   }
 
-//  @Test
-//  public void testASTAR() throws Exception {
-//    final GPSEngine engine = new GPSEngine(problem, SearchStrategy.ASTAR);
-//    engine.findSolution();
-//    Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
-//  }
+  @Test
+  public void testASTAR() throws Exception {
+    final GPSEngine engine = new GPSEngine(problem, SearchStrategy.ASTAR);
+    engine.findSolution();
+    Assert.assertNotNull(engine.getSolutionNode());
+    Assert.assertTrue(engine.getSolutionNode().getState().equals(solvedState));
+  }
 }
